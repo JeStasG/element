a87b2e7a7e8a9fbb8ba0772b3f80af38f843fe45
SGalkin <s_galkin@hwcompany.ru>
2020-07-15 14:52:51 +0300

    add new loader

--- packages/loading/src/loading.vue
+++ packages/loading/src/loading.vue
@@ -2,17 +2,21 @@
   <transition name="el-loading-fade" @after-leave="handleAfterLeave">
     <div
       v-show="visible"
-      class="el-loading-mask"
+      class="el-loading-mask hwc-loading-mask"
       :style="{ backgroundColor: background || '' }"
       :class="[customClass, { 'is-fullscreen': fullscreen }]">
-      <div class="el-loading-spinner">
+      <div class="el-loading-spinner hwc-loader-spinner">
         <!--<svg v-if="!spinner" class="circular" viewBox="25 25 50 50">
           <circle class="path" cx="50" cy="50" r="20" fill="none"/>
         </svg>-->
-        <div v-if="!spinner" class="hwc-spinner">
-        </div>
-        <i v-else :class="spinner"></i>
-        <p v-if="text" class="el-loading-text">{{ text }}</p>
+<!--        <div v-if="!spinner" class="hwc-spinner">-->
+<!--        </div>-->
+<!--        <i v-else :class="spinner"></i>-->
+<!--        <p v-if="text" class="el-loading-text">{{ text }}</p>-->
+        <h1>ЗАГРУЗКА...</h1>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </div>
   </transition>
